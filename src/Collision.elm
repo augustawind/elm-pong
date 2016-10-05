82b1feff6461f4089c990f8a0bf33867facc1c21
Dustin Rohde <dustin.rohde@gmail.com>
2016-10-05 01:06:57 -0400

    Removed unneeded functions.

--- src/Collision.elm
+++ src/Collision.elm
@@ -1,9 +1,6 @@
 module Collision exposing (..)
 
 
-import List
-
-
 type Collision
   = LeftPaddleCollision
   | RightPaddleCollision
@@ -74,71 +71,3 @@ getRightPaddleCollision : BallBox -> RightPaddleBox -> Bool
 getRightPaddleCollision ball paddle =
   (ball.top <= paddle.bottom || ball.bottom >= paddle.top) &&
   (ball.right >= paddle.left && ball.left <= paddle.left)
-
-
-{-
-getBoxCollision : Box -> Box -> Maybe Collision
-getBoxCollision box1 box2 =
-  let
-    cx1 = box1.centerX
-    cy1 = box1.centerY
-    cx2 = box2.centerX
-    cy2 = box2.centerY
-
-    rightCollision = box1.right >= box2.left && box1.left <= box2.left 
-      && abs (cx1 - cx2) >= abs (cy1 - cy2)
-    leftCollision = box1.left <= box2.right && box1.right >= box2.right
-      && abs (cx1 - cx2) >= abs (cy1 - cy2)
-
-    topCollision = box1.top <= box2.bottom && box1.bottom >= box2.bottom
-      && abs (cy1 - cy2) >= abs (cx1 - cx2)
-    bottomCollision = box1.bottom >= box2.top && box1.top <= box2.top
-      && abs (cy1 - cy2) >= abs (cx1 - cx2)
-
-    xCollision = rightCollision || leftCollision
-    yCollision = topCollision || bottomCollision
-  in
-    parseCollision xCollision yCollision
-
-
-getBoundsCollision : Box -> Box -> Maybe Collision
-getBoundsCollision innerBox outerBox =
-  let
-    xCollision = innerBox.left <= outerBox.left || innerBox.right >= outerBox.right
-    yCollision = innerBox.top <= outerBox.top || innerBox.bottom >= outerBox.bottom
-  in
-    parseCollision xCollision yCollision
-
-
-parseCollision : Bool -> Bool -> Maybe Collision
-parseCollision xCollision yCollision =
-  if xCollision && yCollision then
-    Just XYCollision
-  else if xCollision then
-    Just XCollision
-  else if yCollision then
-    Just YCollision
-  else
-    Nothing
-
-
-getCollision : Box -> Box -> List (Box, Box) -> Maybe Collision
-getCollision innerBox outerBox boxes =
-  let
-    boundsCollision = getBoundsCollision innerBox outerBox
-    boxCollisions = List.map (uncurry getBoxCollision) boxes
-  in
-    List.foldr mergeCollisions Nothing (boundsCollision :: boxCollisions)
-
-
-mergeCollisions : Maybe Collision -> Maybe Collision -> Maybe Collision
-mergeCollisions coll1 coll2 =
-  case (coll1, coll2) of
-    (Just XYCollision, _) -> Just XYCollision
-    (_, Just XYCollision) -> Just XYCollision
-    (Just XCollision, Just YCollision) -> Just XYCollision
-    (Just YCollision, Just XCollision) -> Just XYCollision
-    (something, Nothing) -> something
-    (Nothing, something) -> something
-    (something, _) -> something
--}
